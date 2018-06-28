2985998651b06f81151189c600c84d6a25b30894
Dhivaker <40656719+Dhivaker@users.noreply.github.com>
2018-06-28 16:20:00 +0200

    Add files via upload

---
+++ ultrasonic_LEDs.ino
@@ -0,0 +1,49 @@
+
+const int trigPin = 32  ;
+const int echoPin = 37 ; 
+int ledPin =  9;
+void setup()
+{
+  Serial.begin(9600);    
+   pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT); 
+
+}
+
+void loop() {
+   
+ // establish variables for duration of the ping, and the distance result in inches and centimeters:
+long duration;
+// The ultrasound sensor is triggered by a HIGH pulse of 2 or more microseconds. Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+int dis;
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(2);
+digitalWrite(trigPin, LOW);
+delayMicroseconds(10);
+duration = pulseIn(echoPin, HIGH);
+dis = (duration/2)*0.034;   // convert the time into a distance in cm
+if (dis>80)
+{
+  
+  digitalWrite(ledPin, LOW); 
+  
+}
+ else if (dis<20)
+  {
+   
+   digitalWrite(ledPin, HIGH); 
+ 
+  }
+   else
+   {
+   
+    digitalWrite(ledPin, LOW); 
+    
+ }
+ 
+  Serial.println("Distance:");
+Serial.println(dis);
+delay(500);
+}
+
