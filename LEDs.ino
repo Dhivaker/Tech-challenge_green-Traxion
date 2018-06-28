a7ac5a6bd1a2fc3e0f95b0ffe352f418855ea802
Dhivaker <40656719+Dhivaker@users.noreply.github.com>
2018-06-28 16:08:54 +0200

    Add files via upload

---
+++ LEDs.ino
@@ -0,0 +1,20 @@
+int ledPin =  9;
+int buttonPin =  11;
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  int buttonPress;
+  buttonPress = digitalWrite(buttonPin, HIGH);
+  
+  if (buttonPress ==1 )
+  {
+    digitalWrite(ledPin, HIGH);
+   
+  }
+  else
+  digitalWrite(ledPin, LOW);
+}
