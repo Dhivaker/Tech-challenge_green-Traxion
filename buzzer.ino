d94a280c38b76e79ec6f8b5207ab9b7079e0eaf7
Dhivaker <40656719+Dhivaker@users.noreply.github.com>
2018-06-28 16:12:11 +0200

    Add files via upload

---
+++ buzzer.ino
@@ -0,0 +1,20 @@
+int buzzerPin =  9;
+int buttonPin =  11;
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+}
+
+void loop() {
+  int buttonPress;
+  buttonPress = digitalWrite(buzzerPin, HIGH);
+  
+  if (buttonPress ==1 )
+  {
+    digitalWrite(buzzerPin, HIGH);
+   
+  }
+  else
+  digitalWrite(buzzerPin, LOW);
+}
